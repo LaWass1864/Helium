79390c4fbd338403f72b67ec1cb96647521a8b6e
LaWass1864 <amwass4@hotmail.com>
2023-06-13 20:08:36 +0200

    خن

---
+++ index.js
@@ -0,0 +1,21 @@
+// Mouse Events
+
+const greenmouse = document.querySelector('.greenmouse');
+const graymouse = document.querySelector('.graymouse');
+const blackmouse = document.querySelector('.blackmouse');
+const link = document.querySelector("a");
+
+window.addEventListener("mousemove", (e) => {
+    blackmouse.style.left = e.pageX + "px";
+    blackmouse.style.top = e.pageY + "px";   
+    graymouse.style.left = e.pageX + "px";
+    graymouse.style.top = e.pageY + "px";
+    greenmouse.style.left = e.pageX + "px";
+    greenmouse.style.top = e.pageY + "px";
+  });
+
+
+//   window.open("http://google.com", "cours js", "height=600, width=800");
+
+document.addEventListener('mouseover', () => 
+)
